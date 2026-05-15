fb8cbaaa46c832495511bd2f767cf1d908a9dde1
abrancoteams10-creator <abranco.teams10@gmail.com>
2026-05-15 10:46:56 -0300

    Create Gemini.py

---
+++ Gemini.py
@@ -0,0 +1,18 @@
+!pip install google-genai
+
+import os
+from google.colab import userdata
+os.environ["GOOGLE_API_KEY"] = userdata.get('GeminiAPI')
+
+from google import genai
+client = genai.Client()
+MODEL_ID = "gemini-2.5-flash"
+
+from IPython.display import HTML, Markdown
+resposta = client.models.generate_content(
+    model=MODEL_ID,
+    contents='Me diga o o top 5 de claudinho e buxexa, dando nota para as músicas.',
+)
+display(Markdown(f"Resposta:\n {resposta.text}"))
+
+resposta.candidates
