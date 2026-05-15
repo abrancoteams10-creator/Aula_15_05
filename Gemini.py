1c29b521055e2fb705ed026c51afa8a7f8571dff
abrancoteams10-creator <abranco.teams10@gmail.com>
2026-05-15 11:20:54 -0300

    Update Gemini.py

--- Gemini.py
+++ Gemini.py
@@ -1,24 +1,35 @@
-
 import streamlit as st
 from google import genai
 
-# Pega a chave dos secrets do Streamlit
-api_key = st.secrets["GEMINI_API_KEY"]
-
-# Cria o cliente Gemini
-client = genai.Client(api_key=api_key)
-
-MODEL_ID = "gemini-2.5-flash"
+# SENHA DE ACESSO
+SENHA_CORRETA = "1895"
 
 st.title("Teste Gemini")
 
-prompt = st.text_input("Digite algo")
+senha = st.text_input("Digite a senha", type="password")
 
-if st.button("Enviar"):
+if senha == SENHA_CORRETA:
 
-    resposta = client.models.generate_content(
-        model=MODEL_ID,
-        contents=prompt
-    )
+    # Pega a chave dos secrets do Streamlit
+    api_key = st.secrets["GEMINI_API_KEY"]
 
-    st.write(resposta.text)
+    # Cria o cliente Gemini
+    client = genai.Client(api_key=api_key)
+
+    MODEL_ID = "gemini-2.5-flash"
+
+    st.success("Acesso liberado!")
+
+    prompt = st.text_input("Digite algo")
+
+    if st.button("Enviar"):
+
+        resposta = client.models.generate_content(
+            model=MODEL_ID,
+            contents=prompt
+        )
+
+        st.write(resposta.text)
+
+elif senha != "":
+    st.error("Senha incorreta!")
