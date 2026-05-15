bc63d3025911653d0fedf7d6af268e63bdd033e6
abrancoteams10-creator <abranco.teams10@gmail.com>
2026-05-15 11:11:02 -0300

    Update Gemini.py

--- Gemini.py
+++ Gemini.py
@@ -2,41 +2,23 @@
 import streamlit as st
 from google import genai
 
- 
-
 # Pega a chave dos secrets do Streamlit
 api_key = st.secrets["GEMINI_API_KEY"]
-
- 
 
 # Cria o cliente Gemini
 client = genai.Client(api_key=api_key)
 
- 
-
 MODEL_ID = "gemini-2.5-flash"
-
- 
 
 st.title("Teste Gemini")
 
- 
-
 prompt = st.text_input("Digite algo")
-
- 
 
 if st.button("Enviar"):
 
- 
+    resposta = client.models.generate_content(
+        model=MODEL_ID,
+        contents=prompt
+    )
 
-    resposta = client.models.generate_content(
-        model=MODEL_ID,
-        contents=prompt
-    )
-
- 
-
-    st.write(resposta.text)
-
-
+    st.write(resposta.text)
